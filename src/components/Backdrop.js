f2626d5558d98ec0747ce1b4f4cb5117749f327b
Anas Tazir <atazir2018@gmail.com>
2021-10-08 12:29:16 +0530

    give the backdrop a z index

--- src/components/Backdrop.js
+++ src/components/Backdrop.js
@@ -1,20 +1,37 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { motion } from "framer-motion";
 
-const backdropCss= {
-  position: 'absolute',
-  top: '0',
-  left: '0',
-  height: '100%',
-  width: '100%',
-  background: '#000000e1',
-  display: 'flex',
-  alignItems: 'center',
-  justifContent: 'center',
-}
+
 
 const Backdrop = ({ children, onClick }) => {
+  //  TODO :make the backdrop span the entire height of screen(including scroll)
+  // let [windowHeight, setWindowHeight] = useState(0)
 
+  // const handleScroll=()=>{
+  //   let t = window.scrollY
+  //   console.log('hieght changed scroll', window.scrollY);
+  //   setWindowHeight(t)
+  //   console.log('changed scroll to ', windowHeight);
+  // }
+  // window.addEventListener("scroll",handleScroll);
+
+
+  // console.log("scroll height ", window.scrollY)
+  const backdropCss= {
+    position: 'absolute',
+    top: '0',
+    left: '0',
+    height: `${window.scrollY+1000}px`,
+    width: '100%',
+    background: '#000000e1',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    // backgroundRepeat: 'no-repeat',
+    // backgroundSize: 'cover'
+    // backgroundAttachment: 'scroll'
+    zIndex: '2'
+  }
   return (
     <motion.div
       className="backdrop"
