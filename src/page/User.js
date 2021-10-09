2182cd0ff14b62b7e643164861f7df95426e2133
Anas Tazir <atazir2018@gmail.com>
2021-10-09 14:22:33 +0530

    add images to local storage

--- src/page/User.js
+++ src/page/User.js
@@ -1,43 +1,47 @@
 import React, { useContext, useEffect, useRef, useState } from "react";
+import { AnimateSharedLayout, motion } from "framer-motion";
+
 import Image from '../components/image'
-import { AnimatePresence, AnimateSharedLayout, motion } from "framer-motion";
 import AppContext from "../store/AppContext";
+import {addToLocalStorage, checkLocalStorage, getSavedImages} from "../storage/storeImages";
+
 let newSearchImage=''
+
 export default function User() {
 
     const [isLoggedIn,user] = useContext(AppContext)
     const userName= user.email? (user.email.substring(0, user.email.indexOf("@"))) : null;
-
+    
     const [Images, setImages] = useState(['https://images.unsplash.com/photo-1616818400884-1c4f3d4d003c?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMTYyOTB8MHwxfGFsbHw4fHx8fHx8Mnx8MTYxNjg1ODY3OQ&ixlib=rb-1.2.1&q=80&w=400',
-    'https://images.unsplash.com/photo-1616156104743-0ed6f123d8b8?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMTYyOTB8MHwxfGFsbHw5fHx8fHx8Mnx8MTYxNjg1ODY3OQ&ixlib=rb-1.2.1&q=80&w=400','https://images.unsplash.com/photo-1616841888027-89693dec0827?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMTYyOTB8MHwxfGFsbHwxMHx8fHx8fDJ8fDE2MTY4NTg2Nzk&ixlib=rb-1.2.1&q=80&w=400','https://images.unsplash.com/photo-1616732227193-6d556628ede1?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMTYyOTB8MHwxfGFsbHwxMnx8fHx8fDJ8fDE2MTY4NTkxMDM&ixlib=rb-1.2.1&q=80&w=400'
-])
+    'https://images.unsplash.com/photo-1616156104743-0ed6f123d8b8?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMTYyOTB8MHwxfGFsbHw5fHx8fHx8Mnx8MTYxNjg1ODY3OQ&ixlib=rb-1.2.1&q=80&w=400','https://images.unsplash.com/photo-1616841888027-89693dec0827?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMTYyOTB8MHwxfGFsbHwxMHx8fHx8fDJ8fDE2MTY4NTg2Nzk&ixlib=rb-1.2.1&q=80&w=400','https://images.unsplash.com/photo-1616732227193-6d556628ede1?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMTYyOTB8MHwxfGFsbHwxMnx8fHx8fDJ8fDE2MTY4NTkxMDM&ixlib=rb-1.2.1&q=80&w=400'])
+    
     const [showPreview, setShowPreview] = useState(false);
     const inputRef = useRef(null)
+    
     function handleRemove(index){
         setImages(Images.filter((image,i)=>i!==index))
     }
 
     useEffect(() => {
         inputRef.current.focus()
+        if (!checkLocalStorage()) {
+            setImages(getSavedImages(userName))
+        }else{
+            addToLocalStorage(Images, userName)
+        }
     }, [])
+
+    useEffect(() => {
+        addToLocalStorage(Images, userName)
+    },[Images])
     
     function handleInputChange(e){
         newSearchImage=e.target.value
     }
 
-    function handleSearch(){
-        // let tempImages=[...Images]
-        // tempImages.push(newSearchImage)
-        // console.log(newSearchImage);
-        // inputRef.current.value=''
-        // setImages(tempImages)
-        // console.log(tempImages);
-        setImages([
-            ...Images,
-            newSearchImage
-        ])
+    function handleAdd(){
+        setImages([...Images,newSearchImage])
         inputRef.current.value=''
-
     }
 
     function ShowImage(){
@@ -76,9 +80,8 @@ export default function User() {
                     onChange={handleInputChange}></input>
                 </div>
                 <div>
-                    <button className='p-1 bg-green-600 text-white  ' onClick={handleSearch}>Search</button>
+                    <button className='p-1 bg-green-600 text-white  ' onClick={handleAdd}>Add</button>
                 </div>
-                       
             </div>
         </div>
     )
