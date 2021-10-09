2cb6c569cd46f28df697ee285f6cfffc29a1463e
Anas Tazir <atazir2018@gmail.com>
2021-10-09 19:36:35 +0530

    from the createdAt variable only get the date

--- src/components/image.js
+++ src/components/image.js
@@ -4,6 +4,7 @@ import useTFClassify from "../utils/hooks/useTFClassify";
 import Backdrop from './Backdrop';
 
 export default function Image({image, handleRemove, index, description, createdAt}) {
+    const date= createdAt.split('T')  // only get the date and cut out the time
     const imageRef =useRef()
     const [showPreview, setShowPreview] = useState(false)
     const { predict, predictions, setPredictions, isLoading } = useTFClassify();
@@ -36,7 +37,7 @@ export default function Image({image, handleRemove, index, description, createdA
                   exit={{ opacity: 0, rotate: -360, transition: { duration: 0.5 } }}
                   className="fixed w-full h-full flex justify-center items-center top-0 left-0 z-40" onClick={()=>setShowPreview(false)}>
                   <div className="bg-white text-black">
-                    <p className='bg-black text-white '>{createdAt}</p>
+                    <p className='bg-black text-white '>Created on {date[0]}</p>
                     <img className='rounded-md  bg-black' onClick={()=>setShowPreview(false)} src={image} alt="" width='400' height='auto' />
                     <p className="bg-black text-white break-all break-word">{description}</p>
                   </div>
