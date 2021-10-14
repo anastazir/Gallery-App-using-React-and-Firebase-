0dd31bea8d7b4f5c4984f83781370cb094e51b87
Anas Tazir <atazir2018@gmail.com>
2021-10-14 12:13:16 +0530

    show image in modal with new animation

--- src/components/image.js
+++ src/components/image.js
@@ -4,6 +4,7 @@ import { saveImageURL } from '../storage/storeImages';
 import useTFClassify from "../utils/hooks/useTFClassify";
 import Backdrop from './Backdrop';
 import AppContext from "../store/AppContext";
+import Modal from '../components/modal';
 
 export default function Image({image, handleRemove, index, description, createdAt}) {
   const [isLoggedIn,user] = useContext(AppContext)
@@ -36,18 +37,8 @@ export default function Image({image, handleRemove, index, description, createdA
       </div>
       <AnimatePresence>
         {showPreview && 
-          <Backdrop onClick={()=>console.log('working') }>
-            <motion.section 
-              initial={{ opacity: 0 }}
-              animate={{ opacity: 1, rotate: 360, transition: { duration: 0.5 } }}
-              exit={{ opacity: 0, rotate: -360, transition: { duration: 0.5 } }}
-              className="fixed w-full h-full flex justify-center items-center top-0 left-0 z-40" onClick={()=>setShowPreview(false)}>
-              <div className="bg-white text-black">
-                {date && <p className='bg-black text-white '>Created on {date[0]}</p>}
-                <img className='rounded-md  bg-black' onClick={()=>setShowPreview(false)} src={image} alt="" width='400' height='auto' />
-                <p className="bg-black text-white break-all break-word">{description}</p>
-              </div>
-            </motion.section> 
+          <Backdrop onClick={()=>setShowPreview(false) }>
+            <Modal text={description} type='result' data={date} url={image} />             
           </Backdrop>
         }
       </AnimatePresence>
