2108c3bcaf1ba6fe9487aa051719d3f4eec3e8a4
Anas Tazir <atazir2018@gmail.com>
2021-11-10 09:52:47 -0800

    add media queries

--- src/components/Images.js
+++ src/components/Images.js
@@ -24,27 +24,24 @@ export default function Images() {
         setImages(Images.filter((image,i)=>i!==index))
     }
   /////////////////////////////////////
-    function ShowImage(){
-            // initial={{ opacity: 0 }}
-            //       animate={{ opacity: 1 }}
-        return (
-            <div className="flex flex-wrap " >
-                <AnimateSharedLayout type="switch">
-                    {Images.map((image,index)=>(
-                    <motion.div className="xl:w-1/5 p-1 border flex justify-center md:w-1/3 sm:w-1/2"
-                    key={index}
-                    layoutId={index}>
-                        <Image 
-                        createdAt={image.created_at}
-                        description={image.description}
-                        show={() => setShowPreview(index)}
-                        image={image.urls.small} 
-                        handleRemove={handleRemove}
-                        index={index}/>
-                    </motion.div>))}
-                </AnimateSharedLayout>      
-            </div>   
-        )
+function ShowImage(){
+    return (
+    <div className="flex flex-wrap " >
+        <AnimateSharedLayout type="switch">
+            {Images.map((image,index)=>(
+            <motion.div className="xl:w-1/5 p-1 border flex justify-center md:w-1/3 sm:w-1"
+                key={index}
+                layoutId={index}>
+                <Image 
+                createdAt={image.created_at}
+                description={image.description}
+                show={() => setShowPreview(index)}
+                image={image.urls.small} 
+                handleRemove={handleRemove}
+                index={index}/>
+            </motion.div>))}
+        </AnimateSharedLayout>      
+    </div>)
     }
 
     function scrollToTop(){
@@ -68,32 +65,31 @@ export default function Images() {
     return(
     <section>
         <ShowImage/>
-        {/* {console.log(errors)} */}
         {
-            isLoading?<i className="fas fa-circle-notch fa-spin text-7xl" ></i>: (
-                <div>
-                    <div className='w-7 h-7 rounded-2xl fab-con-top' onClick={scrollToTop}>
-                        <i className='align-middle fas fa-arrow-up text-2xl cursor-pointer'></i>
+        isLoading?<i className="fas fa-circle-notch fa-spin text-7xl" ></i>: (
+            <div>
+                <div className='w-7 h-7 rounded-2xl fab-con-top' onClick={scrollToTop}>
+                    <i className='align-middle fas fa-arrow-up text-2xl cursor-pointer'></i>
+                </div>
+                <button className='p-1 bg-green-600 text-white' onClick={() =>setPage(page+1)}>Load More?</button>
+                <div className='flex justify-between my-5'>
+                    <div className='w-full'>
+                        <input
+                        autoComplete='off'
+                        placeholder='             Enter Query'
+                        className='p-2 border-gray-800 shadow rounded bg-black w-full text-center'
+                        ref={inputRef}
+                        id='inputBox'  
+                        type='text'  
+                        onKeyDown={handleKeyDown}
+                        onChange={handleInputChange}></input>
                     </div>
-                    <button className='p-1 bg-green-600 text-white' onClick={() =>setPage(page+1)}>Load More?</button>
-                    <div className='flex justify-between my-5'>
-                        <div className='w-full'>
-                            <input
-                            autoComplete='off'
-                            placeholder='             Enter Query'
-                            className='p-2 border-gray-800 shadow rounded bg-black w-full text-center'
-                            ref={inputRef}
-                            id='inputBox'  
-                            type='text'  
-                            onKeyDown={handleKeyDown}
-                            onChange={handleInputChange}></input>
-                        </div>
-                        <div>
-                            <button className='p-1 bg-green-600 text-white'  onClick={handleSearch}>Search</button>
-                        </div>
+                    <div>
+                        <button className='p-1 bg-green-600 text-white'  onClick={handleSearch}>Search</button>
                     </div>
                 </div>
-            )
+            </div>
+        )
         }
     </section>
     )
