eea2836ec350a15dfb14c1a5416c112d3b97ddb1
Anas Tazir <atazir2018@gmail.com>
2022-04-22 20:23:43 +0530

    change class to className

--- src/page/Home.js
+++ src/page/Home.js
@@ -7,7 +7,7 @@ export default function Home() {
             <motion.h1 initial={{opacity:0,y:-400}} animate={{ scale: 1.5,opacity:1,y:0,transition:{duration:1},rotate:720 }} className="m-auto text-3xl">
                 Home
             </motion.h1>
-            <div class='fixed bottom-5 left-5 w-full'>
+            <div className='fixed bottom-5 left-5 w-full'>
                 <a target="_blank" style={{ backgroundColor: "black"}} href="https://github.com/anastazir/Gallery-App-using-React-and-Firebase-" rel="noreferrer">
                     <img src="https://img.shields.io/badge/github-%23121011.svg?style=for-the-badge&logo=github&logoColor=white" />
                 </a>
