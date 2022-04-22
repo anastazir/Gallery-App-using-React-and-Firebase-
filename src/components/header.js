08741cd6d396208d481a628d15f16b542834de1c
Anas Tazir <atazir2018@gmail.com>
2022-04-22 20:33:00 +0530

    use className instead of activeClassName

--- src/components/header.js
+++ src/components/header.js
@@ -1,9 +1,8 @@
 import React, { useContext}  from 'react'
-import {NavLink,useNavigate } from 'react-router-dom';
+import { NavLink, useNavigate } from 'react-router-dom';
 import firebase from '../config/firebase'
 import AppContext from '../store/AppContext';
 export default function Header() {
-  // const [isLoggedIn, setIsLoggedIn] = useState(false)
   const history = useNavigate ();
   const [isLoggedIn,user] = useContext(AppContext)
   const userName= user.email? (user.email.substring(0, user.email.indexOf("@"))) : null;
@@ -11,7 +10,6 @@ export default function Header() {
 
   function logout(){
     firebase.auth().signOut().then(res =>{
-      // setIsLoggedIn(false);
       history.replace('/login')
     }).catch(e=>{
       console.log(e);
@@ -20,29 +18,29 @@ export default function Header() {
   return (
     <nav className='py-5 sticky top-0 z-10 bg-black'>
       <ul className="flex justify-between px-10">
-      <span className="flex">
-        <li  className="mr-5">
-          <NavLink exact={true} activeClassName="underline text-blue-200" to="/">Home</NavLink>
-        </li>
-        <li className="mr-5">
-          <NavLink exact={true} activeClassName="underline text-blue-200" to="/gallery">Gallery</NavLink>
-        </li>
-        {/* <li className="mr-5">
-          <NavLink exact={true} activeClassName="underline text-blue-200" to="/tensorflow">Tensorflow</NavLink>
-        </li> */}
+        <span className="flex">
+          <li  className="mr-5">
+            <NavLink exact={true} className={(navData) => (navData.isActive ? "underline text-blue-200" : 'none')} to="/">Home</NavLink>
+          </li>
+          <li className="mr-5">
+            <NavLink exact={true} className={(navData) => (navData.isActive ? "underline text-blue-200" : 'none')} to="/gallery">Gallery</NavLink>
+          </li>
+          {/* <li className="mr-5">
+            <NavLink exact={true} className={(navData) => (navData.isActive ? "underline text-blue-200" : 'none')} to="/tensorflow">Tensorflow</NavLink>
+          </li> */}
         </span>
         <span className="flex">
-        <li className="mr-5">
-        {
-          isLoggedIn ? ([<button className="mx-5" onClick={logout}>Logout</button>,
-          <NavLink exact={true} activeClassName="underline text-blue-200" to="/user">{userName}</NavLink>]):(<NavLink exact={true} activeClassName="underline text-blue-200" to="/login">Login</NavLink>)
-        }
-        </li>
-        <li>
-        {
-          isLoggedIn? null :(<NavLink exact={true} activeClassName="underline text-blue-200" to="/signup" >Sign up</NavLink>)
-        }
-        </li>
+          <li className="mr-5">
+            {
+              isLoggedIn ? ([<button className="mx-5" onClick={logout}>Logout</button>,
+              <NavLink exact={true} className={(navData) => (navData.isActive ? "underline text-blue-200" : 'none')} to="/user">{userName}</NavLink>]):(<NavLink exact={true} activeClassName="underline text-blue-200" to="/login">Login</NavLink>)
+            }
+          </li>
+          <li>
+            {
+              isLoggedIn? null :(<NavLink exact={true} className={(navData) => (navData.isActive ? "underline text-blue-200" : 'none')} to="/signup" >Sign up</NavLink>)
+            }
+          </li>
         </span>
       </ul>
     </nav>
