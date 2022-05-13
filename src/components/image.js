97bd53473b6594890be58476be81ab90066d192c
Anas Tazir <atazir2018@gmail.com>
2022-05-14 02:35:45 +0530

    hide add icon in /user path

--- src/components/image.js
+++ src/components/image.js
@@ -1,5 +1,7 @@
+import {  useLocation } from 'react-router-dom';   
 import { AnimatePresence } from 'framer-motion'
 import React, { useContext, useRef,useState } from 'react'
+import { useNavigate } from 'react-router-dom';
 import { saveImageURL } from '../storage/storeImages';
 import Backdrop from './Backdrop';
 import AppContext from "../store/AppContext";
@@ -12,15 +14,25 @@ export default function Image({image, handleRemove, index, description, createdA
   const date= createdAt ? createdAt.split('T') : null // only get the date and cut out the time
   const imageRef =useRef()
   const [showPreview, setShowPreview] = useState(false)
+  const location = useLocation();
+  const inUser = location.pathname === "/user";
 
   return (
     <div>
       <div className="relative">
-        <i className="fa fa-download  absolute right-50 cursor-pointer opacity-25 hover:opacity-100 hidden" onClick={()=>{downloadImage(imageLarge, imageId)}} ></i>
-        <i className="fa fa-plus  absolute left-0 cursor-pointer opacity-25 hover:opacity-100 hidden" onClick={()=>{saveImageURL(image, userName)}}></i>
-        <i className="fa fa-times absolute right-0 cursor-pointer opacity-25 hover:opacity-100 hidden" onClick={()=>{handleRemove(index)}} ></i>
-        <i className="fa fa-clipboard absolute bottom-0 right-50 cursor-pointer opacity-25 hover:opacity-100 hidden" onClick={(e)=>navigator.clipboard.writeText(image)}></i>
-        <img ref={imageRef} onClick={()=>setShowPreview(true)} src={image} alt="" width='100%' height='auto' crossOrigin='anonymous' />
+        <i className="fa fa-download  absolute right-50 cursor-pointer opacity-25 hover:opacity-100 hidden" 
+          onClick={()=>{downloadImage(imageLarge, imageId)}} ></i>
+
+        {!inUser && <i className="fa fa-plus  absolute left-0 cursor-pointer opacity-25 hover:opacity-100 hidden" 
+          onClick={()=>{saveImageURL(image, userName)}}></i>}
+
+        <i className="fa fa-times absolute right-0 cursor-pointer opacity-25 hover:opacity-100 hidden" 
+          onClick={()=>{handleRemove(index)}} ></i>
+
+        <i className="fa fa-clipboard absolute bottom-0 right-50 cursor-pointer opacity-25 hover:opacity-100 hidden" 
+          onClick={(e)=>navigator.clipboard.writeText(image)}></i>
+        <img ref={imageRef} 
+          onClick={()=>setShowPreview(true)} src={image} alt="" width='100%' height='auto' crossOrigin='anonymous' />
       </div>
       <AnimatePresence>
         {showPreview && 
